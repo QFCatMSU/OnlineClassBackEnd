548ee8216d4856d1a5d133c9ea665c80afdfdd84
Charlie <belinsky@msu.edu>
2018-05-01 12:36:43 -0400

    using js to store values

--- header.js
+++ header.js
@@ -1,27 +1,14 @@
-function createHeader()
-{
-	document.body.innerHTML += "Sdfsdaf";
-	newDiv = document.createElement("div");
-	newDiv.innerHTML = "sdfsadfsaf";
-	newDiv.style.top = 0;
-	newDiv.style.left = 0;
-	newDiv.style.width = "500px";
-	newDiv.style.height = "300px";
-	newDiv.style.position = "absolute";
-	newDiv.style.backgroundColor = "red";
-	document.body.appendChild(newDiv);
-	/*	alert(3);
-	xmlhttp=new XMLHttpRequest();
-	xmlhttp.open("GET","header.xml",true);
-	xmlhttp.timeout = 2000; // time in milliseconds
-	
-	xmlhttp.onload = function()
-	{
-		alert(1);
-		var xmlDoc = this.responseXML;
-	};
-	
-	xmlhttp.ontimeout = function (e) {
-	  alert(2);// XMLHttpRequest timed out. Do something here.
-	};*/
-}
+var lessonLinks = new Array();
+var lessonNames = new Array();
+
+lessonNames[0] = "aaaa";
+lessonNames[1] = "bbbb";
+lessonNames[2] = "cccc";
+lessonNames[3] = "dddd";
+lessonNames[4] = "eeee";
+
+lessonLinks[0] = "http://aaaa.com";
+lessonLinks[1] = "http://bbbb.com"";
+lessonLinks[2] = "http://cccc.com"";
+lessonLinks[3] = "http://dddd.com"";
+lessonLinks[4] = "http://eeee.com"";
