65be73d627389964704164edbe120fd8b5af0280
belinskyc <36079242+belinskyc@users.noreply.github.com>
2018-09-17 16:21:05 -0400

    Move latex change to onload

--- module.js
+++ module.js
@@ -105,12 +105,6 @@ scrollTopPosition = 0; 			// value saved for links-return-links within a page
 returnLink = null;				// element on page that contains the return link
 overflowCalled = false;   		// check to see if there is a current check of code lines
 
-// remove display="block" from <math> objects -- only Latex equations
-m = document.getElementsByTagName("math");
-for(i=0; i<m.length; i++)
-{
-	m[i].setAttribute("display", "inline");
-}
 
 addStyleSheet();  // can be done before page load since this is called in the [head]
 	
@@ -147,6 +141,14 @@ parent.window.onload = function()
 	encapObject = document.body;  
 	editURL = "";
 
+	
+// remove display="block" from <math> objects -- only Latex equations
+m = document.getElementsByTagName("math");
+for(i=0; i<m.length; i++)
+{
+	m[i].setAttribute("display", "inline");
+}
+	
 		// check if we are in Joomla or D2L
 	
    // check if any meta content starts with "Joomla"
